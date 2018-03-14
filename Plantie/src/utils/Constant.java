89c9138a95dde74899a519ba40044767295af527
michaelhoeller <michael.hoeller@fiprox.com>
2018-03-14 18:18:42 +0100

    deletion

--- Plantie/src/utils/Constant.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Constant {
-    
-    public final static String SEPARATOR_VALUES = ";";
-    public final static String SEPARATOR_MIDDLE = "|";
-    public final static String SEPARATOR_EOL    = "?";
-    public static final String FILENAME         = "res/data.txt";
-}
