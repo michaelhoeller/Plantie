50965ee3db1601ee2d51e6eef887a89fa0f9db07
michaelhoeller <michael.hoeller@fiprox.com>
2018-03-14 18:19:17 +0100

    waea

--- Plantie/src/gui/MainPage.java
+++ Plantie/src/gui/MainPage.java
@@ -1,42 +1,77 @@
 package gui;
 
-import java.awt.Toolkit;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import gui.helper.LayoutHelper;
 
 public class MainPage extends JFrame {
     
     private static final long serialVersionUID = 1L;
-    private JButton           btnSelectDocument;
-    private JLabel            lblDocumentPath;
+    private JMenuBar          mBar;
+    private JMenu             mnMode;
+    private JMenuItem         mntmStart;
+    private JMenuItem         mntmMan;
+    private JMenu             mnAuto;
+    private JMenuItem         mntmConfiguration;
     
     public MainPage() {
-        setTitle("Plantie");
-        this.setDefaultCloseOperation(3);
-        this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getClassLoader().getResource("res/icon2.png")));
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        getContentPane().setLayout(null);
-        getContentPane().add(getBtnSelectDocument());
-        getContentPane().add(getLblDocumentPath());
+        init();
+        new LayoutHelper(this, "Plantie");
         this.setVisible(true);
     }
     
-    private JButton getBtnSelectDocument() {
-        if (btnSelectDocument == null) {
-            btnSelectDocument = new JButton("Choose Document");
-            btnSelectDocument.setBounds(291, 239, 143, 23);
-        }
-        return btnSelectDocument;
+    private void init() {
+        setJMenuBar(getMBar());
     }
     
-    private JLabel getLblDocumentPath() {
-        if (lblDocumentPath == null) {
-            lblDocumentPath = new JLabel("New label");
-            lblDocumentPath.setBounds(10, 243, 180, 14);
+    private JMenuBar getMBar() {
+        if (mBar == null) {
+            mBar = new JMenuBar();
+            mBar.add(getMnMode());
         }
-        return lblDocumentPath;
+        return mBar;
+    }
+    
+    private JMenu getMnMode() {
+        if (mnMode == null) {
+            mnMode = new JMenu("Mode");
+            mnMode.add(getMnAuto());
+            mnMode.add(getMntmMan());
+        }
+        return mnMode;
+    }
+    
+    private JMenuItem getMntmStart() {
+        if (mntmStart == null) {
+            mntmStart = new JMenuItem("Start");
+            mntmStart.setEnabled(false);
+        }
+        return mntmStart;
+    }
+    
+    private JMenuItem getMntmMan() {
+        if (mntmMan == null) {
+            mntmMan = new JMenuItem("Manuell");
+        }
+        return mntmMan;
+    }
+    
+    private JMenu getMnAuto() {
+        if (mnAuto == null) {
+            mnAuto = new JMenu("Atutomatic");
+            mnAuto.add(getMntmStart());
+            mnAuto.add(getMntmConfiguration());
+        }
+        return mnAuto;
+    }
+    
+    private JMenuItem getMntmConfiguration() {
+        if (mntmConfiguration == null) {
+            mntmConfiguration = new JMenuItem("Configuration");
+        }
+        return mntmConfiguration;
     }
 }
--- Plantie/src/main/Main.java
+++ Plantie/src/main/Main.java
@@ -1,8 +1,13 @@
 package main;
 
+import core.FileReader;
+import core.MeasurementHolder;
+import gui.LineChartCreator;
+
 public class Main {
     
     public static void main(String[] args) throws Exception {
-        Tester.generateData(20);
+        // Tester.generateData(50);
+        new LineChartCreator("Test", new MeasurementHolder(new FileReader().getFileString()).getHoldingList());
     }
 }
